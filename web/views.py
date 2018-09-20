9a92b396d4c1f9cb7950d5ebab23fbec5c42c4b1
Brian Savatia Keyonzo <briankeyonzo@yahoo.com>
2018-09-21 02:11:36 +0300

    [ADD] Connected backend with frontend

--- web/views.py
+++ web/views.py
@@ -1,5 +1,10 @@
+from django.core import serializers
 from django.shortcuts import render
 
 # Create your views here.
+from core.models import Building
+
+
 def index(request):
-    return render(request, 'web/index.html')
+    buildings = Building.objects.all()
+    return render(request, 'web/index.html', context= {'buildings': buildings})
