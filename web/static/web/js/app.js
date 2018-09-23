9d9dc608f53816fb8a08598aad5bf1796ab6a40e
Brian Savatia Keyonzo <briankeyonzo@yahoo.com>
2018-09-24 00:56:25 +0300

    A mess

--- web/static/web/js/app.js
+++ web/static/web/js/app.js
@@ -1,3 +1,15 @@
 /**************************************
- * Leaflet JS setup                   *
- **************************************/
+* Leaflet JS setup                   *
+**************************************/
+Building = function(title, lat, lon, featureCollection, image){
+    this.title = title;
+    this.latitude = lat;
+    this.longitude = lon;
+    this.image = lon;
+    this.featureCollection = featureCollection;
+    this.image = image
+
+    this.log = function(){
+        console.log(this.title);
+    }
+}
--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 from web import views
 
 urlpatterns = [
+
     path('', views.index),
+    path('buidlings/<int:building_id>/rooms', views.rooms_edit),
+    path('api/buildings/<int:building_id>/indoor', views.buildling_api_indoor, name="buildling_api_indoor"),
+    path('rooms', views.rooms_create, name="rooms_create"),
 ]
