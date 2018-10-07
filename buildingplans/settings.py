a92ae9de0c34fa02f87c648e01cc63a0f48f9954
kiruti <ronald.tunui@gmail.com>
2018-10-07 11:04:53 +0300

    change some few

--- buildingplans/settings.py
+++ buildingplans/settings.py
@@ -97,7 +97,6 @@ DATABASES['default'] = DATABASES['sqlite3'] if os.getenv('DATABASE_ENGINE', None
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
